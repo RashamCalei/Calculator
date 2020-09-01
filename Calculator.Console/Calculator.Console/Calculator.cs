38a53010e334654f69ea4ee150ac96595658866a
RashamCalei <rog.rasham@gmail.com>
2020-09-01 23:54:02 +0200

    Initial commit

---
+++ Calculator.Console/Calculator.Console/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Console
+{
+    class Calculator
+    {       
+        float firstValue;       // tworzenie pól klasy
+        float secondValue;
+        float result;
+        public float Add(float a, float b)      // dodawanie // argumenty do wprowadzenia przez użytkownika      
+        {
+            SetFirstValue(a);        // zapisanie podanych wartości w polach obiektu
+            SetSecondValue(b);    
+            result = a + b;     
+            return result;      // zwrot wyniku
+        }
+        public float Substract(float a, float b)        // odejmowanie
+        {
+            SetFirstValue(a);       
+            SetSecondValue(b);
+            result = a - b;
+            return result;
+        }
+        public float Multiply(float a, float b)     // mnożenie
+        {
+            SetFirstValue(a);       
+            SetSecondValue(b);
+            result = a * b;
+            return result;
+        }
+        public float Divide(float a, float b)       // dzielenie
+        {
+            SetFirstValue(a);        
+            SetSecondValue(b);
+            result = a / b;
+            return result;
+        }
+        public void SetFirstValue( float value)     // ustawienie wartości pola firstValue
+        {
+            this.firstValue = value;
+        }
+        public void SetSecondValue(float value)     // ustawienie wartości pola secondValue
+        {
+            this.secondValue = value;
+        }
+        public void ShowResult()        // z góry ustawiony znak działania nie da rady
+        {
+            System.Console.WriteLine($"{firstValue} + {secondValue} = {result}");
+        }
+    }
+}
---
+++ Calculator.Console/Calculator.Console/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Console
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Calculator calc = new Calculator();
+            calc.Add(Parser(), Parser());
+            calc.ShowResult();
+            calc.Substract(Parser(), Parser());
+            calc.ShowResult();
+            calc.Multiply(Parser(), Parser());
+            calc.ShowResult();
+            calc.Divide(Parser(), Parser());
+            calc.ShowResult();
+        }
+        public static float Parser()
+        {
+            labelParse:;
+            string input;
+            float number = 0;
+        
+            try
+            {
+                input = System.Console.ReadLine();
+                number = float.Parse(input);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Wprowadzono niepoprawną wartość.");
+                goto labelParse;
+            }
+            return number;
+        }
+    }
+}
